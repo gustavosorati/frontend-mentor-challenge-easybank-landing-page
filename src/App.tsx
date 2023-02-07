158b881023b71668747b70c6007f18408376b2f4
Gustavo Sorati <sorati.gustavo@gmail.com>
2023-02-06 21:28:48 -0300

    fix: change main component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
+import { Header } from './components/Header'
+import { Home } from './pages/Home'
+import { Hero } from './pages/Home/components/Hero'
+import { GlobalStyles } from './styles/theme'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Home />
+
+      <GlobalStyles />
     </div>
   )
 }
