022789ed6feb65caf5234cebf4442f26cd874909
Gustavo Sorati <sorati.gustavo@gmail.com>
2023-02-06 21:27:24 -0300

    feat: add global style

---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Public Sans', sans-serif;
+    font-weight: 400;
+  }
+
+  a {
+    all: unset;
+  }
+  
+`
